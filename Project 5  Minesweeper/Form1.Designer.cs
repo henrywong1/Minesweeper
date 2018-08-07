907a29fcb2d6c77442800c3ae98f346cace19480
Henrywong1 <h_wong14@u.pacific.edu>
2018-08-07 03:26:24 -0700

    Minesweeper Game

---
+++ Project 5  Minesweeper/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Project_5__Minesweeper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(179, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 402);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(214, 603);
+            this.label1.MaximumSize = new System.Drawing.Size(500, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 40);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Henry\'s Minesweeper";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 690);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Project 5  Minesweeper/Form1.cs
@@ -0,0 +1,226 @@
+﻿//Henry's Minesweeper
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Project_5__Minesweeper
+{
+    public partial class Form1 : Form
+    {
+        Button[,] board = new Button[10, 10];
+
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            //Create 10x10 board of buttons
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    board[i, j] = new Button();
+                    board[i, j].Location = new Point(i * 40, j * 40);
+                    board[i, j].Width = 40;
+                    board[i, j].Height = 40;
+                    board[i, j].Click += buttonArray_Click;
+                    groupBox1.Controls.Add(board[i, j]);
+                }
+            }
+            //Generates mines
+            Random rnd = new Random();
+            int xvalue = rnd.Next(10);
+            int yvalue = rnd.Next(10);
+            int count = 0;
+            while (count < 11)
+            {
+                board[xvalue, yvalue].Tag = "*";
+                xvalue = rnd.Next(10);
+                yvalue = rnd.Next(10);
+                count++;
+            }
+        }
+        
+        private void buttonArray_Click(object sender, EventArgs e)
+        {
+            Button temp = (Button)sender;
+            int curCol = (temp.Location.Y) / 40;
+            int curRow = (temp.Location.X) / 40;
+            //Clicked on mine
+            //winner();
+            if (board[curRow, curCol].Tag == "*")
+            {
+                MessageBox.Show("You landed on a Mine!");
+                for (int i = 0; i < 10; i++)
+                {
+                    for (int j = 0; j < 10; j++)
+                    {
+                        if (board[i, j].Tag == "*")
+                        {
+                            board[i, j].Text = "*";
+                        }
+                        board[i,j].Enabled = false;       
+                    }
+                }
+            }
+            // showing # 1
+            // for values not in the last rows/cols
+            if (board[curRow, curCol].Tag != "*" && curCol > 0 && curCol < 9 && curRow > 0 && curRow < 9)
+            {
+                if (board[curRow + 1, curCol].Tag == "*"
+                || board[curRow, curCol + 1].Tag == "*"
+                || board[curRow + 1, curCol + 1].Tag == "*"
+                || board[curRow - 1, curCol].Tag == "*"
+                || board[curRow, curCol - 1].Tag == "*"
+                || board[curRow - 1, curCol - 1].Tag == "*"
+                || board[curRow + 1, curCol - 1].Tag == "*"
+                || board[curRow - 1, curCol + 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            // first column
+            if (board[curRow, curCol].Tag != "*" && curRow == 0 && curCol < 9 && curCol > 0)
+            {
+                if (board[curRow + 1, curCol + 1].Tag == "*"
+                    || board[curRow + 1, curCol].Tag == "*"
+                    || board[curRow, curCol + 1].Tag == "*"
+                    || board[curRow, curCol - 1].Tag == "*"
+                    || board[curRow + 1, curCol - 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            //last col
+            if (board[curRow, curCol].Tag != "*" && curRow > 0 && curRow == 9 && curCol < 9 && curCol > 0)
+            {
+                if (board[curRow, curCol + 1].Tag == "*"
+                    || board[curRow - 1, curCol].Tag == "*"
+                    || board[curRow, curCol - 1].Tag == "*"
+                    || board[curRow - 1, curCol - 1].Tag == "*"
+                    || board[curRow - 1, curCol + 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            // first row
+            if (board[curRow, curCol].Tag != "*" && curRow > 0 && curRow < 9 && curCol == 0 && curCol < 9)
+            {
+                if (board[curRow + 1, curCol].Tag == "*"
+                    || board[curRow, curCol + 1].Tag == "*"
+                    || board[curRow - 1, curCol].Tag == "*"
+                    || board[curRow + 1, curCol + 1].Tag == "*"
+                    || board[curRow - 1, curCol + 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            // last row
+            if (board[curRow, curCol].Tag != "*" && curRow > 0 && curRow < 9 && curCol > 0 && curCol == 9)
+            {
+                if (board[curRow + 1, curCol].Tag == "*"
+                     || board[curRow - 1, curCol].Tag == "*"
+                     || board[curRow, curCol - 1].Tag == "*"
+                     || board[curRow - 1, curCol - 1].Tag == "*"
+                     || board[curRow + 1, curCol - 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            // corners
+            if (board[curRow, curCol].Tag != "*" && curRow == 0 && curCol == 0)
+            {
+                if (board[curRow + 1, curCol].Tag == "*"
+                    || board[curRow + 1, curCol + 1].Tag == "*"
+                    || board[curRow, curCol + 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            if (board[curRow, curCol].Tag != "*" && curRow == 0 && curCol == 9)
+            {
+                if (board[curRow + 1, curCol].Tag == "*"
+                    || board[curRow, curCol - 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            if (board[curRow, curCol].Tag != "*" && curRow == 9 && curCol == 0)
+            {
+                if (board[curRow, curCol + 1].Tag == "*"
+                    || board[curRow - 1, curCol + 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            if (board[curRow, curCol].Tag != "*" && curRow == 9 && curCol == 9)
+            {
+                if (board[curRow, curCol - 1].Tag == "*"
+                    || board[curRow - 1, curCol].Tag == "*"
+                    || board[curRow - 1, curCol - 1].Tag == "*")
+                {
+                    temp.Text = "1";
+                    temp.Enabled = false;
+                }
+            }
+            else
+                temp.Enabled = false;
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        //void winner()
+        //{
+        //    for (int i = 0; i < 10; i++)
+        //    {
+        //        for (int j = 0; j < 10; j++)
+        //        {
+        //            if (board[i,j].Text != "*")
+        //            {
+        //                MessageBox.Show("You Win!");
+        //            }
+        //        }
+        //    }
+        //}
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    board[i, j].Enabled = true;
+                    board[i, j].Tag = "";
+                    board[i, j].Text = "";
+                }
+            }
+            Random rnd = new Random();
+            int xvalue = rnd.Next(10);
+            int yvalue = rnd.Next(10);
+            int count = 0;
+            while (count < 11)
+            {
+                board[xvalue, yvalue].Tag = "*";
+                xvalue = rnd.Next(10);
+                yvalue = rnd.Next(10);
+                count++;
+            }
+        }
+    }
+}
